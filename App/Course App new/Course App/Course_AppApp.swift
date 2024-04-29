7d1db06f9ea4fcb17954d5deab1574834b0bf44c
matesfila <matesfila@gmail.com>
2024-04-29 16:43:08 +0200

    #du1
    vytvorenie prazdneho projektu

---
+++ App/Course App new/Course App/Course_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Course_AppApp.swift
+//  Course App
+//
+//  Created by Matúško on 29/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Course_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
